eeccf4aaf4508f9f0e425ae71c8766dbb92a292c
sasmitapraveen <sasmitapraveen@gmail.com>
2020-04-14 20:45:29 -0500

    updated but still not proper

--- Missions_to_Mars/scraping_mars.py
+++ Missions_to_Mars/scraping_mars.py
@@ -4,62 +4,43 @@ from splinter import Browser
 import pandas as pd
 import time
 
-def init_browser():
+# Set Executable Path & Initialize Chrome Browser
 
-    executable_path = {'executable_path': 'chromedriver.exe'}
-    return Browser('chrome', **executable_path, headless=False)
-
-def scrape():
+executable_path = {"executable_path": "./chromedriver.exe"}
+browser = Browser("chrome", **executable_path)
 
 
-    mars_data ={}
 
-    mars_data['news']= mars_news()
-    mars_data['featured_image_url'] = mars_image()
-    mars_data['mars_weather'] = mars_weather()
-    mars_data['mars_fact']=mars_fact_table()
-    mars_data["mars_hemispheres"] = hemisphereImage()
+def mars_news(browser):
 
-    return mars_data
-
-def mars_news():
-
-    browser = init_browser()
-
+    
     # URL of page to be scraped\n"
-    url = 'https://mars.nasa.gov/news/'
+    url = 'http://mars.nasa.gov/news/'
     browser.visit(url)
 
-    time.sleep(2)
+    time.sleep(10)
  
-    # Retrieve page with the requests module
-    response = requests.get(url)
+  
 
     # Create BeautifulSoup object; parse with 'html.parser
-    soup = BeautifulSoup(response.text, 'html.parser')
+    html = browser.html
+    soup = BeautifulSoup(html, "html.parser")
 
-    news ={}
+    li_element = soup.select_one("ul.item_list li.slide")
+    li_element.find("div", class_="content_title")
 
-    # Extract latest news title text
-    results = soup.find('div', class_='content_title')
-    news_title = results.a.text.strip()
+    news_title = li_element.find("div", class_="content_title").get_text()
 
-    # Extract latest paragraph text
-    paragraph = soup.find('div', class_='image_and_description_container')
-    news_p = paragraph.a.text.strip()
+    news_p = li_element.find("div", class_="article_teaser_body").get_text()
+
     
-    news['news_title'] = news_title
-    news['paragraph_text'] = news_p
+    return news_title, news_p
 
-    browser.quit()
-    time.sleep(5)
-    return news
+def mars_image(browser):
 
-def mars_image():
+   
 
-    browser = init_browser()
-
-    jpl_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    jpl_url = 'http://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
 
     browser.visit(jpl_url)
 
@@ -68,12 +49,13 @@ def mars_image():
 
     browser.find_by_id("full_image").click()
 
-    time.sleep(10)
+    time.sleep(5)
     
 
     browser.find_link_by_partial_text("more info").click()
  
-    time.sleep(10)
+    time.sleep(5)
+
     html = browser.html
     # Parse HTML with Beautiful Soup
     soup = BeautifulSoup(html, 'html.parser')
@@ -84,20 +66,15 @@ def mars_image():
     new_url = '/'.join(jpl_url.split('/')[:3])
     featured_image_url = f'{new_url}{img_url}'
 
-
-    browser.quit()
-    time.sleep(5)
-
     return featured_image_url
 
 
-def mars_weather():
+def mars_weather(browser):
 
-    browser = init_browser()
-  
-    url = 'https://twitter.com/marswxreport?lang=en'
+    url = 'http://twitter.com/marswxreport?lang=en'
     browser.visit(url)
     time.sleep(2)
+
     html1 = browser.html
     # Parse HTML with Beautiful Soup
     soup1 = BeautifulSoup(html1, 'html.parser')
@@ -109,54 +86,30 @@ def mars_weather():
     mars_span= mars_tweet_section.find_all('span')[4].text
     mars_weather=','.join(mars_span.splitlines())
 
-
-    browser.quit()
-    time.sleep(5)
-   
     return mars_weather
 
 def  mars_fact_table():
 
-    browser = init_browser()
-
-    url = 'https://space-facts.com/mars/'
-
-    browser.visit(url)
-    time.sleep(2)
-    # HTML object
-    html2= browser.html\
-    # Parse HTML with Beautiful Soup
-    soup2 = BeautifulSoup(html2, 'html.parser')
-    # Retrieve mars fact table
-    mars_fact_table = soup2.find('table', class_='tablepress tablepress-id-p-mars')
-    
-    keys=[]
-    values=[]
-    mars_table_col_1 = mars_fact_table.find_all(class_='column-1')
-    mars_table_col_2 = mars_fact_table.find_all(class_='column-2')
-
-    for num in range(9):
-        keys.append(mars_table_col_1[num].text)
-        values.append(mars_table_col_2[num].text)
-  
-    table_df = pd.DataFrame(values,keys)
-    facts_table = table_df.to_html('mars_fact_table.html')
     
 
-    browser.quit()
-    time.sleep(5)
+    url = 'http://space-facts.com/mars/'
+    
+
+    df = pd.read_html(url)[0]
+    df.columns=["Description", "Value"]
+    df.set_index("Description", inplace=True)
+    facts_table= df.to_html(classes="table")
+
+
+
 
     return facts_table
 
-def hemisphereImage():
+def hemisphereImage(browser):
 
-    browser = init_browser()
-
-  
-    url='https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    url='http://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(url)
-    time.sleep(2)
-   
+       
     hemisphere_image_urls = []
    
     # Get a List of All the Hemispheres
@@ -180,17 +133,30 @@ def hemisphereImage():
     
         # Navigate Backwards
         browser.back()
-
-
-        browser.quit()
-        time.sleep(5)
+        
 
 
     return hemisphere_image_urls
 
 
 
+def scrape():
 
+    executable_path = {"executable_path": "./chromedriver.exe"}
+    browser = Browser("chrome", **executable_path)
+
+    mars_data ={}
+    news_title, news_p = mars_news(browser)
+    mars_data['news_title']= news_title
+    mars_data['news_p']= news_p
+    mars_data['featured_image_url'] = mars_image(browser)
+    mars_data['mars_weather'] = mars_weather(browser)
+    mars_data['mars_fact']=mars_fact_table()
+    mars_data["mars_hemispheres"] = hemisphereImage(browser)
+
+    browser.quit()
+
+    return mars_data
 
 
 
